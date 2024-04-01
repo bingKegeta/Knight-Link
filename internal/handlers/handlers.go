eee5689311a6dd12e4cf6433572744fb58e0764b
Soham Ganguly <sohamg24@gmail.com>
2024-04-01 19:43:45 -0400

    Create the first draft of the API route handlers

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,28 +1,98 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/go-chi/render"
 )
 
-type Order struct{}
-
-func (o *Order) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Create an order")
+type User struct {
+	UID         int    `json:"uid"`
+	Name        string `json:"name"`
+	Email       string `json:"email"`
+	Login       string `json:"login"`
+	Password    string `json:"password"`
+	DateCreated string `json:"dateCreated"`
+	Desc        string `json:"desc"`
+	Authority   string `json:"authority"`
 }
 
-func (o *Order) List(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Lists all orders")
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the logic to get a user
+	render.JSON(w, r, "GetUser endpoint")
 }
 
-func (o *Order) GetByID(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Gets by ID")
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the logic to delete a user
+	render.JSON(w, r, "DeleteUser endpoint")
 }
 
-func (o *Order) UpdateByID(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Update an order by ID")
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the logic to update a user
+	render.JSON(w, r, "UpdateUser endpoint")
 }
 
-func (o *Order) DeleteByID(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Delete an order by ID")
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the logic to create a user
+	render.JSON(w, r, "CreateUser endpoint")
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the logic to login a user
+	render.JSON(w, r, "Login endpoint")
+}
+
+func Logout(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the logic to logout a user
+	render.JSON(w, r, "Logout endpoint")
+}
+
+func GetAllEvents(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the logic to get all events
+	render.JSON(w, r, "GetAllEvents endpoint")
+}
+
+func GetEvent(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the logic to get an event
+	render.JSON(w, r, "GetEvent endpoint")
+}
+
+func DeleteEvent(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the logic to delete an event
+	render.JSON(w, r, "DeleteEvent endpoint")
+}
+
+func UpdateEvent(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the logic to update an event
+	render.JSON(w, r, "UpdateEvent endpoint")
+}
+
+func CreateEvent(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the logic to create an event
+	render.JSON(w, r, "CreateEvent endpoint")
+}
+
+func GetAllRSOs(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the logic to get all RSOs
+	render.JSON(w, r, "GetAllRSOs endpoint")
+}
+
+func GetRSO(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the logic to get an RSO
+	render.JSON(w, r, "GetRSO endpoint")
+}
+
+func DeleteRSO(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the logic to delete an RSO
+	render.JSON(w, r, "DeleteRSO endpoint")
+}
+
+func UpdateRSO(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the logic to update an RSO
+	render.JSON(w, r, "UpdateRSO endpoint")
+}
+
+func CreateRSO(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the logic to create an RSO
+	render.JSON(w, r, "CreateRSO endpoint")
 }
