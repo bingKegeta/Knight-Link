764a8a32d08a79b4c6a26621da93a1a99ca158be
Soham Ganguly <sohamg24@gmail.com>
2024-04-02 13:44:35 -0400

    Rework API routes and handlers to match new SQL setup script

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -6,6 +6,7 @@ import (
 	"github.com/go-chi/render"
 )
 
+// * Probably need to add more structs to make the JSONs easier to make(?)
 type User struct {
 	UID         int    `json:"uid"`
 	Name        string `json:"name"`
@@ -16,6 +17,8 @@ type User struct {
 	Desc        string `json:"desc"`
 	Authority   string `json:"authority"`
 }
+
+//! Remember to set the status codes
 
 func GetUser(w http.ResponseWriter, r *http.Request) {
 	// TODO: Implement the logic to get a user
@@ -95,4 +98,69 @@ func UpdateRSO(w http.ResponseWriter, r *http.Request) {
 func CreateRSO(w http.ResponseWriter, r *http.Request) {
 	// TODO: Implement the logic to create an RSO
 	render.JSON(w, r, "CreateRSO endpoint")
+}
+
+func AttendEvent(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the logic to add user to event list
+	render.JSON(w, r, "AttendEvent endpoint")
+}
+
+func UnattendEvent(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the logic to remove user from event list
+	render.JSON(w, r, "UnattendEvent endpoint")
+}
+
+func CreateFeedback(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the logic to create comment and rating from a user to an event
+	render.JSON(w, r, "CreateFeedback endpoint")
+}
+
+func JoinRSO(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the logic to allow user to join an RSO
+	render.JSON(w, r, "JoinRSO endpoint")
+}
+
+func LeaveRSO(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the logic to allow user to leave an RSO
+	render.JSON(w, r, "LeaveRSO endpoint")
+}
+
+func GetAllUnis(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the logic to get all Universities
+	render.JSON(w, r, "GetAllUnis endpoint")
+}
+
+func GetUni(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the logic to get a specific university by id(?)
+	render.JSON(w, r, "GetUni endpoint")
+}
+
+func DeleteUni(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the logic to delete a University
+	render.JSON(w, r, "DeleteUni endpoint")
+}
+
+func UpdateUniDetails(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the logic to update the details of a specific university
+	render.JSON(w, r, "UpdateUniDetails endpoint")
+}
+
+func CreateUni(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the logic to create a university
+	render.JSON(w, r, "CreateUni endpoint")
+}
+
+func JoinUni(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the logic to allow a user to join a university
+	render.JSON(w, r, "JoinUni endpoint")
+}
+
+func LeaveUni(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the logic to allow a user to leave a university
+	render.JSON(w, r, "LeaveUni endpoint")
+}
+
+func GetLocations(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the logic to get all the locations of a user in a given radius
+	render.JSON(w, r, "GetLocations endpoint")
 }
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -23,6 +23,9 @@ func Routes() *chi.Mux {
 		r.Mount("/api/auth", AuthRoutes())
 		r.Mount("/api/events", EventRoutes())
 		r.Mount("/api/rsos", RSORoutes())
+		r.Mount("/api/unis", UniRoutes())
+		r.Mount("/api/locations", LocationRoutes())
+		// Add new route groups here
 	})
 
 	return router
@@ -34,6 +37,8 @@ func UserRoutes() http.Handler {
 	router.Delete("/{userId}", handlers.DeleteUser)
 	router.Put("/{userId}", handlers.UpdateUser)
 	router.Post("/", handlers.CreateUser)
+
+	// Add new user-related endpoints here (e.g., update profile picture)
 	return router
 }
 
@@ -41,25 +46,58 @@ func AuthRoutes() http.Handler {
 	router := chi.NewRouter()
 	router.Post("/login", handlers.Login)
 	router.Post("/logout", handlers.Logout)
+
+	// Add new auth-related endpoints here (e.g., refresh token)
 	return router
 }
 
 func EventRoutes() http.Handler {
 	router := chi.NewRouter()
-	router.Get("/", handlers.GetAllEvents)
+	router.Get("/", handlers.GetAllEvents) // Assuming filtering is implemented in handler
 	router.Get("/{eventId}", handlers.GetEvent)
 	router.Delete("/{eventId}", handlers.DeleteEvent)
 	router.Put("/{eventId}", handlers.UpdateEvent)
 	router.Post("/", handlers.CreateEvent)
+
+	// Add new event-related endpoints here (e.g., attend/unattend event, submit feedback)
+	router.Post("/{eventId}/attend", handlers.AttendEvent)      // Example for attending an event
+	router.Delete("/{eventId}/attend", handlers.UnattendEvent)  // Example for unattending an event
+	router.Post("/{eventId}/feedback", handlers.CreateFeedback) // Example for submitting feedback
 	return router
 }
 
 func RSORoutes() http.Handler {
 	router := chi.NewRouter()
-	router.Get("/", handlers.GetAllRSOs)
+	router.Get("/", handlers.GetAllRSOs) // Assuming filtering is implemented in handler
 	router.Get("/{rsoId}", handlers.GetRSO)
 	router.Delete("/{rsoId}", handlers.DeleteRSO)
 	router.Put("/{rsoId}", handlers.UpdateRSO)
 	router.Post("/", handlers.CreateRSO)
+
+	// Add new RSO-related endpoints here (e.g., join/leave RSO)
+	router.Post("/{rsoId}/join", handlers.JoinRSO)    // Example for joining an RSO
+	router.Delete("/{rsoId}/join", handlers.LeaveRSO) // Example for leaving an RSO
+	return router
+}
+
+func UniRoutes() http.Handler {
+	router := chi.NewRouter()
+	router.Get("/", handlers.GetAllUnis) // Assuming filtering is implemented in handler
+	router.Get("/{uni_id}", handlers.GetUni)
+	router.Delete("/{uni_id}", handlers.DeleteUni)
+	router.Put("/{uni_id}", handlers.UpdateUniDetails)
+	router.Post("/{uni_id}", handlers.CreateUni)
+
+	// Add new Uni-related endpoints here (e.g. join/leave Uni)
+	router.Post("/{uni_id}/join", handlers.JoinUni)
+	router.Delete("/{uni_id}/join", handlers.LeaveUni)
+	return router
+}
+
+func LocationRoutes() http.Handler {
+	router := chi.NewRouter()
+	router.Get("/", handlers.GetLocations) // Assuming all visible events in a given radius is shown
+
+	// Add other routes as required (e.g. add/delete locations)
 	return router
 }
